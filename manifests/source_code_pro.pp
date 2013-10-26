c6efc8a5a2c6217dd902d1c2faacb1c6f05b3d44
Arden Packeer <contactme@ardenpackeer.com>
2013-10-26 22:00:39 +1100

    Updated URL

--- manifests/source_code_pro.pp
+++ manifests/source_code_pro.pp
@@ -5,7 +5,7 @@
 #   include fonts::source_code_pro
 class fonts::source_code_pro {
   zipped_font { 'SourceCodePro':
-    url     => 'http://sourceforge.net/projects/sourcecodepro.adobe/files/latest/download?source=files',
+    url     => 'http://sourceforge.net/projects/sourcecodepro.adobe/files/SourceCodePro_FontsOnly-1.017.zip/download',
     creates => 'SourceCodePro-Black.ttf'
   }
 }
